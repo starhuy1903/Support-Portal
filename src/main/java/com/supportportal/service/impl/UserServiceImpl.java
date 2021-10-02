92110a47b762bb5d9558a09890a73b51223cf76a
huy190302 <huykst137@gmail.com>
2021-10-02 20:37:03 +0700

    Completed user Details Service Implementation

---
+++ src/main/java/com/supportportal/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.supportportal.service.impl;
+
+import com.supportportal.domain.User;
+import com.supportportal.domain.UserPrincipal;
+import com.supportportal.repository.UserRepository;
+import com.supportportal.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+@Transactional
+@Qualifier("UserDetailsService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if(user == null) {
+            LOGGER.error("User not found by username: " + username);
+            throw new UsernameNotFoundException("User not found by username: " + username);
+        } else {
+            user.setLastLoginDateDisplay(user.getLastLoginDate());
+            user.setLastLoginDate(new Date());
+            userRepository.save(user);
+            UserPrincipal userPrincipal = new UserPrincipal(user);
+            LOGGER.info("Returning found user by username");
+
+            return userPrincipal;
+        }
+    }
+}
