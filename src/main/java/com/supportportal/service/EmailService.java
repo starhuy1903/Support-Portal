6e5c01398466d5b5597038b5203a982e8eb937d9
huy190302 <huykst137@gmail.com>
2021-10-03 20:51:57 +0700

    Added Email Content

--- src/main/java/com/supportportal/service/EmailService.java
+++ src/main/java/com/supportportal/service/EmailService.java
@@ -2,13 +2,31 @@ package com.supportportal.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.Properties;
 
 import static com.supportportal.constant.EmailConstant.*;
 
 @Service
 public class EmailService {
+
+    private Message createEmail(String firstName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello " + firstName + ", \n \n Your new account password is: " + password + "\n \n The Support Team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
 
     private Session getEmailSession() {
         Properties properties = System.getProperties();
