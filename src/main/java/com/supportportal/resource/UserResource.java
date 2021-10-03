b5d3cfc2016d6c31780853bca40d8284a737dfae
huy190302 <huykst137@gmail.com>
2021-10-03 07:51:07 +0700

    Completed Exception Handling

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,15 +1,19 @@
 package com.supportportal.resource;
 
+import com.supportportal.exception.domain.EmailExistException;
+import com.supportportal.exception.domain.ExceptionHandling;
+import com.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
+@RequestMapping(path = {"/","/user"})
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String showUser(){
-        return "application works";
+    public String showUser() throws UserNotFoundException {
+//        return "application works";
+        throw new UserNotFoundException("The user was not found");
     }
 }
